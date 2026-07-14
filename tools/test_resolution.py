e392806a7905261f114efec52c56d14768651e88
Choriso <danilafeshencko@gmail.ru>
2026-07-14 11:24:36 +0400

    add search test

--- tools/test_resolution.py
+++ tools/test_resolution.py
@@ -9,8 +9,11 @@
 """
 
 import logging
+import os
 import sys
 from typing import Optional
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from data import session as db_session
 from config import config
